de0d53a95d47e1b44c844d4a7e93848245bda14e
jmorr122 <john.spencer.morris@gmail.com>
2014-03-26 19:54:57 -0400

    Revert "Version 1.2"
    This reverts commit d487fb255c493b47363d6cde4765b3bf3cc77742.

--- WorkoutApp/src/com/example/workoutapp/Exercise.java
+++ WorkoutApp/src/com/example/workoutapp/Exercise.java
@@ -1,5 +1,7 @@
 package com.example.workoutapp;
 
+import java.util.ArrayList;
+
 public class Exercise {
 	
 	private String exerciseName;
--- WorkoutApp/src/com/example/workoutapp/WorkoutApp.java
+++ WorkoutApp/src/com/example/workoutapp/WorkoutApp.java
@@ -1,26 +1,23 @@
-
 package com.example.workoutapp;
 
+import android.*;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.view.Menu;
 
-public class WorkoutApp extends Activity{
-	
+public class WorkoutApp extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.title_screen);
-		//Move to workout layout.
-		
-		Intent intent = new Intent("com.example.workoutapp.WorkoutActivity");
-		startActivity(intent);
-		this.finish();
-		
+		//super.onCreate(savedInstanceState);
+		//setContentView(R.layout.main);
 	}
 
-		
-
-	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
