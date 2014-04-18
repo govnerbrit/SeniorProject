f73f76d7576cda85a7402e0a61a3818aba9976cd
govnerbrit <s.a.cown@gmail.com>
2014-04-18 18:43:19 -0400

    Added count down timer functionality. Known bug- does not display name and time until duration-1.

--- src/com/example/workoutapp/PlayWorkoutActivity.java
+++ src/com/example/workoutapp/PlayWorkoutActivity.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -17,23 +17,27 @@ import android.os.CountDownTimer;
 
 public class PlayWorkoutActivity extends Activity {
 
+	private static final String TAG = "PlayWorkoutActivity";
 	private Workout pwInfo;
 	public static List<Exercise> exerciseList = new ArrayList<Exercise>();
-	public static Exercise firstExercise;
+	public static Exercise currentExercise;
 	private TextView pwExerciseTV, pwCountdownTV, pwTitleOfWorkoutTV;
 	private Button pwStartBTN, pwSkipBTN;
 	private String FILE; //Current playing Exercise Filepath
+	private int currentExerciseNum;
+	private long exerciseStart;
+	private CountDownTimer countDownTimer;
 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.play_workout);
 		String pwTitle = getIntent().getExtras().getString(WorkoutActivity.TITLE_EXTRA);
 		pwInfo = WorkoutActivity.getWorkoutInfo(pwTitle);
 		exerciseList = pwInfo.getExerciseList();
-		firstExercise = exerciseList.get(0);
+		currentExercise = exerciseList.get(0);
 		
 		pwStartBTN = (Button)findViewById(R.id.pwStartBTN);
 		pwStartBTN.setOnClickListener(startButtonListener);
@@ -42,13 +46,20 @@ public class PlayWorkoutActivity extends Activity {
 		
 		// NEED TO SET THE EXERCISES AND FIGURE A WAY TO RUN IT.
 		pwTitleOfWorkoutTV = (TextView)findViewById(R.id.pwTitleOfWorkoutTV);
-		pwTitleOfWorkoutTV.setText(pwInfo.getWorkoutName());
 		pwExerciseTV = (TextView)findViewById(R.id.pwExerciseTV);
-		pwExerciseTV.setText(firstExercise.getExerciseName().toString());
 		pwCountdownTV = (TextView)findViewById(R.id.pwCountdownTV);
-		pwCountdownTV.setText(firstExercise.getDuration() + "");
+
+		/*for(Exercise e: exerciseList){
+			currentExercise = e;
+			pwTitleOfWorkoutTV.setText(pwInfo.getWorkoutName());
+			pwExerciseTV.setText(currentExercise.getExerciseName().toString());
+			pwCountdownTV.setText(currentExercise.getDuration() + "");
+			
+		}*/
 		
-		FILE = ""; // call eInfo.getFilePath()
+		createCountdownTimer(0, exerciseList.get(0).getDuration() * 1000);
+		
+		//FILE = ""; // call eInfo.getFilePath()
 	}
 	
 	private OnClickListener startButtonListener = new OnClickListener() {
@@ -69,6 +80,40 @@ public class PlayWorkoutActivity extends Activity {
 		}
 		
 	};
+	
+	public void createCountdownTimer(int exerciseNum, long timeMsec){
+		Log.d(TAG, "createCountdownTimer: enter");
+		currentExerciseNum = exerciseNum;
+		currentExercise = exerciseList.get(currentExerciseNum);
+		exerciseStart = System.currentTimeMillis();
+		String currentExerciseName = currentExercise.getExerciseName().toString();
+		pwExerciseTV.setText(currentExerciseName);
+		String currentSec = Math.round(timeMsec/1000) + "";
+		pwCountdownTV.setText(currentSec);
+			
+		Log.d(TAG, "createCountdownTimer: creating timer:");
+		countDownTimer = new CountDownTimer(timeMsec, 1000){
+
+			@Override
+			public void onFinish() {
+				if(currentExerciseNum < exerciseList.size() - 1){
+					createCountdownTimer(currentExerciseNum + 1, 
+							exerciseList.get(currentExerciseNum + 1).getDuration() * 1000);
+				}
+				
+			}
+
+			@Override
+			public void onTick(long timeLeft) {
+				Log.d(TAG, "onTick called: " + timeLeft + " msec left");
+				pwCountdownTV.setText(Math.round(timeLeft/1000) + "");				
+				
+			}
+			
+		};
+		
+		countDownTimer.start();
+	}
 
 	public void playVoice()
 	{
